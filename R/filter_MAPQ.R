dde1cba3434f1ba1293b6d4414463f0f49e4d41a
Andy Zeng <8595145+andygxzeng@users.noreply.github.com>
2019-01-18 18:01:53 -0500

    Filter MAPQ for both ref and alt alleles. Keep ref alleles with MAPQ == 0 because those are likely homozygous alt.
    This function will be deprecated soon.

--- R/filter_MAPQ.R
+++ R/filter_MAPQ.R
@@ -3,7 +3,9 @@
 #' Filter variants to keep those that are equal to or greater than a MAPQ cutoff.
 #'
 #' @param varscan_output \code{VRanges} object of the varscan pileup2cns output
-#' @param MAPQ_cutoff Minimum acceptable MAPQ score for variants to keep
+#' @param MAPQ_cutoff_ref Minimum acceptable MAPQ score for reference allele
+#' @param MAPQ_cutoff_alt Minimum acceptable MAPQ score for alternate allele
+#' @export
 # @importClassesFrom VariantAnnotation VRanges
 # @importMethodsFrom BiocGenerics which
 #' @examples
@@ -13,8 +15,13 @@
 #' @return This function returns a \code{VRanges} object filtered by MAPQ cutoff
 
 filter_MAPQ <-
-function(varscan_output, MAPQ_cutoff = 59) {
-  varscan_output <- varscan_output[BiocGenerics::which(varscan_output$MapQual2 >= MAPQ_cutoff),]
+function(varscan_output, MAPQ_cutoff_ref = 59, MAPQ_cutoff_alt = 59) {
+
+  # remove variant MAPQs below cutoff
+  varscan_output <- varscan_output[BiocGenerics::which(varscan_output$MapQual2 >= MAPQ_cutoff_alt),]
+
+  # remove ref MAPQs below cutoff but keep MAPQ==0 (homozygous var)
+  varscan_output <- varscan_output[BiocGenerics::which(varscan_output$MapQual1 >= MAPQ_cutoff_ref | varscan_output$MapQual1 == 0),]
 
   return(varscan_output)
 }
