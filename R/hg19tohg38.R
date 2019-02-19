c17bce095f1f256be131cd2e6cf8b5fb8586925b
Andy Zeng <8595145+andygxzeng@users.noreply.github.com>
2019-02-19 13:28:32 -0500

    Removal of liftOver tracks to convert between hg19 and hg38 - this is no longer necessary as the user can just select the hg19 or hg38 specific MAF annotation file

--- R/hg19tohg38.R
+++
@@ -1,7 +0,0 @@
-#' hg19 to hg38 liftOver chain
-#'
-#' Chain file for conversion from hg19 to hg38 through liftOver
-#'
-#' @format \code{Chain} object specifying conversion coordinates for each chromosome
-#' @source Downloaded from UCSC genome browser http://hgdownload.cse.ucsc.edu/downloads.html
-"hg19tohg38"
--- R/hg38tohg19.R
+++
@@ -1,7 +0,0 @@
-#' hg38 to hg19 liftOver chain
-#'
-#' Chain file for conversion from hg38 to hg19 through liftOver
-#'
-#' @format \code{Chain} object specifying conversion coordinates for each chromosome
-#' @source Downloaded from UCSC genome browser http://hgdownload.cse.ucsc.edu/downloads.html
-"hg38tohg19"
