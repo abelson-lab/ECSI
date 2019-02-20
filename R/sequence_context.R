05cf77a06820af126268dfb751521350284d7be3
Andy Zeng <8595145+andygxzeng@users.noreply.github.com>
2019-02-19 23:58:23 -0500

    Updated sequence context to accommodate hg38 annotation

--- R/sequence_context.R
+++ R/sequence_context.R
@@ -3,29 +3,31 @@
 #' Get sequence context for a \code{VRanges} object of the varscan pileup2cns output
 #'
 #' @param varscan_output \code{VRanges} object of the varscan pileup2cns output
+#' @param genome Genome of input object, accepts either hg19 or hg38
 #' @param context Number of nucleotides to return. For example, 3 would return three nucleotides with the position of the variant being the middle nucleotide.
-# @importClassesFrom VariantAnnotation VRanges
-# @importMethodsFrom VariantAnnotation
-# @importMethodsFrom Biostrings getSeq subseq
-# @importFrom BSgenome.Hsapiens.UCSC.hg19 BSgenome.Hsapiens.UCSC.hg19
 #' @export
 #' @examples
 #' \dontrun{
 #' variants <- load_as_VRanges(sample_name = "pt123",
-#' sample_path = "./patient_123_pileup2cns", genome = "hg19", metadata = TRUE)
+#'     sample_path = "./patient_123_pileup2cns", genome = "hg19", metadata = TRUE)
 #' variants <- sequence_context(varscan_output = variants, context = 3)
 #' }
 #' @return This function returns the \code{VRanges} object with the additional metadata column \code{context}.
 
 ### TO DO LIST:
 #	- Check that input is VRanges
-#	- Accommodate hg38 as well
+#	- Perhaps implement genome auto-detection
 
 sequence_context <-
-function(varscan_output, context = 3) {
+function(varscan_output, genome = c("hg19", "hg38"), context = 3) {
 
-  # Get the substitution and sequence context
-  varscan_output$context <- Biostrings::getSeq(BSgenome.Hsapiens.UCSC.hg19::BSgenome.Hsapiens.UCSC.hg19, varscan_output + (context - 1)/2)
+  if(genome == "hg19"){
+    # Get the substitution and sequence context
+    varscan_output$context <- Biostrings::getSeq(BSgenome.Hsapiens.UCSC.hg19::BSgenome.Hsapiens.UCSC.hg19, varscan_output + (context - 1)/2)
+  } else if(genome == "hg38"){
+    # Get the substitution and sequence context
+    varscan_output$context <- Biostrings::getSeq(BSgenome.Hsapiens.UCSC.hg38::BSgenome.Hsapiens.UCSC.hg38, varscan_output + (context - 1)/2)
+  }
 
   # Creating FlankingSeqGroup
       # C > T with 5' A and 3' G would be represented as "A[CT]G".
